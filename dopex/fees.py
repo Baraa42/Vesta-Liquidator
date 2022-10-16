b0c43381454845a7e6472affee4e48a8febfd3a5
Baraa42 <75384314+Baraa42@users.noreply.github.com>
2022-10-16 14:00:20 -0500

    cleanup

--- dopex/fees.py
+++
@@ -1,7 +0,0 @@
-# Dopex fees
-# Option Purchase
-# 0.02% of the underlying asset (Capped at 10% of the total options value)
-# Option Swap
-# 0.03% of the underlying asset (Capped at 10% of the total options value)
-# Option Exercise
-# 0.01% of the underlying asset (Capped at 10% of the total options value)
